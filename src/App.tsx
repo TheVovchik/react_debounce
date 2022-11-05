acaeddf573851037218ae0ea407524996e73ff81
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-11-05 14:03:37 +0200

    UI was modified, function arguments was modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
 import React, { useCallback, useState } from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+
 
 type Func = React.Dispatch<React.SetStateAction<string>>;
 
 function debouncer(f: Func, delay: number) {
-  let timerId: NodeJS.Timeout;
+  let timerId: number;
 
-  return (arg: string) => {
+  return (...arg: any) => {
     clearTimeout(timerId);
-    timerId = setTimeout(f, delay, arg);
+    timerId = setTimeout(f, delay, ...arg);
   }
 }
 
+
+
 function App() {
-  const [text, setText] = useState('example');
+  const [text, setText] = useState('');
   const [appliedText, setAppliedText] = useState(text);
 
   const applyText = useCallback(debouncer(setAppliedText, 1000), []);
 
+  function handleTextInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+    applyText(event.target.value);
+  }
+
   return (
     <div className="App">
-      <h1>Your text will appear here:</h1>
-      <p>{appliedText}</p>
-      <input 
-        id="input1" 
+      <h1 className="title is-1">Your text will appear here:</h1>
+      <p className="subtitle is-4">{appliedText || 'your text will appear here'}</p>
+      <input
+        className="input is-danger is-rounded m-r-5"
+        style={{ 'width': '33%'}}
+        id="text"
+        placeholder='type text here'
         value={text}
-        onChange={event => {
-          setText(event.target.value);
-          applyText(event.target.value);
-        }}
+        onChange={handleTextInput}
         />
     </div>
   );
